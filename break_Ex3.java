f527ae945a9e90de0a71795a789f36b10b26f3e4
SOPHIE LITTON - STUDENT <G00359956@gmit.ie>
2018-02-20 13:50:23 +0000

    week 20 lab ex3

---
+++ break_Ex3.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class break_Ex3{
+  public static void main(String[] args){
+   Scanner input = new Scanner(System.in);
+   int counter = 0;
+
+   System.out.println("Guess a number from 1 to 10");
+   System.out.println("You have 10 attempts:");
+
+   while(input.hasNext()){
+     counter++;
+     if((input.nextInt() ==8)||(counter ==10)){
+     System.out.println("Game over");
+     break;
+    }
+  }
+ }
+}
